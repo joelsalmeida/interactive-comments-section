74fef56b82f49d4f9cb6ebfa137b989b6a378e99
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-04-30 14:28:45 -0300

    fix: ajusta model e migration de Users. cria associacao de Users com Comments e Replies

--- back-end/src/migrations/20220429194400-create-user.js
+++ back-end/src/migrations/20220429194400-create-user.js
@@ -2,6 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
+      id: { primaryKey: true, autoIncrement: true, allowNull: false, type: Sequelize.INTEGER },
       userName: { field: 'user_name', type: Sequelize.STRING, allowNull: false },
       imagePng: { field: 'image_png', type: Sequelize.STRING, allowNull: false },
       imageWebp: { field: 'image_webp', type: Sequelize.STRING, allowNull: false },
--- back-end/src/models/user.js
+++ back-end/src/models/user.js
@@ -1,9 +1,17 @@
 const user = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userName: { field: 'user_name', type: DataTypes.STRING, allowNull: false },
-    imagePng: { field: 'image_png', type: DataTypes.STRING, unique: true, allowNull: false },
-    imageWebp: { field: 'image_webp', type: DataTypes.STRING, unique: true, allowNull: false },
+    userName: { field: 'user_name', type: DataTypes.STRING, unique: true, allowNull: false },
+    imagePng: { field: 'image_png', type: DataTypes.STRING, allowNull: false },
+    imageWebp: { field: 'image_webp', type: DataTypes.STRING, allowNull: false },
   }, { tableName: 'Users', underscored: true, timestamps: false });
+
+  User.associate = (models) => {
+    User.hasMany(models.Comment, 
+      { foreignKey: 'user_id', as: 'comments' });
+        
+    User.hasMany(models.Reply, 
+    { foreignKey: 'user_id', as: 'replies' });
+  };
 
   return User;
 };
